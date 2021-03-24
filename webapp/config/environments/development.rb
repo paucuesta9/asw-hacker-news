8bdfa4aaf5e79fe167441483fe7f019342febfd9
joanvilarrasa <joan.vilarrasa96@gmail.com>
2021-03-24 21:19:45 +0000

    initial users commit

--- webapp/config/environments/development.rb
+++ webapp/config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  
+  config.hosts << "b2c82eb91292458780d1079a6c2c948b.vfs.cloud9.us-east-1.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
