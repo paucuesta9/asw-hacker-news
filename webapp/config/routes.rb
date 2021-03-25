9c293125439ac600e85420b1658722e610f8eb5b
Pau Cuesta <pau.cuesta@gmail.com>
2021-03-25 15:10:31 +0100

    Change routes

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   resources :users
-  get 'home/helloworld'
+  get '/newest', to: 'posts#newest'
+  root :to => 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
