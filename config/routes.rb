9314c4795ee8d8293ec7f6a4cb5aee4765af3bcf
Pau Cuesta <pau.cuesta@gmail.com>
2021-04-08 20:04:04 +0200

    Funciona i creat comentari

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :posts
   resources :users
   get '/newest', to: 'posts#newest'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_132333) do
+ActiveRecord::Schema.define(version: 2021_04_08_180316) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "commentOf"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
