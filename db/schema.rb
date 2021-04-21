5d29128c700d6905028ba55a12874f313daf8ede
Pau Cuesta <pau.cuesta@gmail.com>
2021-04-21 16:16:22 +0200

    upvoted comments user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_113254) do
+ActiveRecord::Schema.define(version: 2021_04_21_141215) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_113254) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", default: 1, null: false
     t.integer "post_id", default: 1, null: false
+    t.integer "votes", default: 0, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -41,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_113254) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "votes", default: 0, null: false
     t.index ["parent_type", "parent_id"], name: "index_replies_on_parent"
     t.index ["user_id"], name: "index_replies_on_user_id"
   end
