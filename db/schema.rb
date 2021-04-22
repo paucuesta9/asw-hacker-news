2486cad70509930f78e0f36587a2bb06024a2039
joanvilarrasa <joan.vilarrasa96@gmail.com>
2021-04-22 16:13:21 +0000

    can edit user about

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_210841) do
+ActiveRecord::Schema.define(version: 2021_04_22_154614) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2021_04_21_210841) do
     t.integer "votes", default: 0, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "g_users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_g_users_on_email", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
@@ -54,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_210841) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "uid"
     t.string "provider"
+    t.string "about"
   end
 
   create_table "vote_comments", force: :cascade do |t|
