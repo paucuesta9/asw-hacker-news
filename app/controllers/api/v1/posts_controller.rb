e7726c888afb86608f934527c20e1df82bf49a44
Pau Cuesta <pau.cuesta@gmail.com>
2021-05-10 12:09:19 +0200

    Get Post with errors

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::PostsController < ApplicationController
     def show
-        @post = Post.select(:id, :title, :url, :text, :points, :user_id, :created_at).find(params[:postId])
-        respond_to do |format|
-        format.json { render json: @post}
+        if Post.exists?(:id => params[:postId])
+            @post = Post.select(:id, :title, :url, :text, :points, :user_id, :created_at).find(params[:postId])
+            respond_to do |format|
+                format.json { render json: @post, status: 200}
+            end
+        else
+            respond_to do |format|
+                format.json { render json: {status: 404, error: 'Not found', message: "No Post with that ID"}, status: 404 }
+            end
         end
     end
 end
