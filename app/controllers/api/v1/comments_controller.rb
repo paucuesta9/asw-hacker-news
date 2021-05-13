bf45dcf81e360a74e7a4bf3a7be324b1eed23809
Ubuntu <ubuntu@ip-172-31-12-242.ec2.internal>
2021-05-13 15:16:37 +0000

    eeeeeeh

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,26 +1,75 @@
 class Api::V1::CommentsController < ApplicationController
   skip_before_action :authenticate, :verify_authenticity_token
     # GET /vote_comments or /vote_comments.json
-  
     def upvote
       if !request.headers["HTTP_X_API_KEY"].nil?
         @user = User.find_by(:uid =>  request.headers["HTTP_X_API_KEY"])
-        @comment = Comment.find(params[:id])
-        VoteComment.create(user_id: @user.id, comment_id: @comment.id)
-        @comment.votes +=1
-        @comment.save
-        redirect_to request.referrer
+        if(@user.nil?)
+          respond_to do |format|
+            format.json { render json: {status: 403, error: 'Forbidden', message: "Your api key (X-API-KEY Header) is not valid"}, status: 403 }
+          end
+        else
+          @comment = Comment.find_by(id: params[:id])
+          if(@comment.nil?)
+              respond_to do |format|
+                format.json { render json: {status: 404, error: 'Not found', message: "No Reply with that ID"}, status: 404 }
+              end
+          else
+            @vote = VoteComment.find_by(reply_id: params[:id], user_id: @user.id)
+            if(!@vote.nil?)
+              respond_to do |format|
+                format.json { render json: {status: 409, error: 'Conflict', message: "Vote to the same Contribution already created"}, status: 409 }
+              end
+            else
+          VoteComment.create(user_id: @user.id, comment_id: @comment.id)
+          @comment.votes +=1
+          @comment.save
+          respond_to do |format|
+                    format.json { render json: @comments, status: 201}
+                end
+            end
+          end
+        end
+      else
+        respond_to do |format|
+          format.json { render json: {status: 401, error: 'Unauthorized', message: "You provided no api key (X-API-KEY Header)"}, status: 401 }
+        end
       end
     end
   
     def unvote
       if !request.headers["HTTP_X_API_KEY"].nil?
         @user = User.find_by(:uid =>  request.headers["HTTP_X_API_KEY"])
-        @comment = Comment.find(params[:id])
-        VoteComment.find_by(user_id: @user.id, comment_id: @comment.id).destroy
-        @comment.votes -= 1
-        @comment.save
-        redirect_to request.referrer
+        if(@user.nil?)
+          respond_to do |format|
+            format.json { render json: {status: 403, error: 'Forbidden', message: "Your api key (X-API-KEY Header) is not valid"}, status: 403 }
+          end
+        else
+          @comment = Comment.find_by(id: params[:id])
+          if(@comment.nil?)
+              respond_to do |format|
+                format.json { render json: {status: 404, error: 'Not found', message: "No Comment with that ID"}, status: 404 }
+              end
+          else
+            @vote = VoteComment.find_by(reply_id: params[:id], user_id: @user.id)
+            if(@vote.nil?)
+              respond_to do |format|
+                format.json { render json: {status: 409, error: 'Conflict', message: "No Vote to the Contribution exists"}, status: 409 }
+              end
+            else
+          VoteComment.create(user_id: @user.id, comment_id: @comment.id).destroy
+          @comment.votes -=1
+          @comment.save
+          respond_to do |format|
+                    format.json { render json: @comments, status: 201}
+                end
+            end
+          end
+        end
+      else
+        respond_to do |format|
+          format.json { render json: {status: 401, error: 'Unauthorized', message: "You provided no api key (X-API-KEY Header)"}, status: 401 }
+        end
       end
     end
   
