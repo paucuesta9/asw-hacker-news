e162ce5afb03210d822e64ffef32a8b241627844
joanvilarrasa <joan.vilarrasa96@gmail.com>
2021-05-13 11:48:30 +0000

    ...

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,13 +3,13 @@ class Api::V1::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if !@user.nil?
-        respond_to do |format|
-          format.json { render json: @user, status: 200}
-        end
+      respond_to do |format|
+        format.json { render json: @user, status: 200}
+      end
     else
-        respond_to do |format|
-          format.json { render json: {status: 404, error: 'Not found', message: "No User with that ID"}, status: 404 }
-        end
+      respond_to do |format|
+        format.json { render json: {status: 404, error: 'Not found', message: "No User with that UID"}, status: 404 }
+      end
     end
   end
 
@@ -19,7 +19,7 @@ class Api::V1::UsersController < ApplicationController
       if @user.save
         format.json { render json: @user, status: 200}
       else
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: {status: 409, error: 'Conflict', message: "User with the same uid already created"}, status: 409 }
       end
     end
   end
@@ -35,7 +35,11 @@ class Api::V1::UsersController < ApplicationController
             format.json { render json: @user.errors, status: :unprocessable_entity }
           end
         end
+      else
+        format.json { render json: {status: 404, error: 'Not found', message: "No User with that UID"}, status: 404 }
       end
+    else
+      format.json { render json: {status: 401, error: 'Unauthorized', message: "You provided no api key (X-API-KEY Header)"}, status: 401 }
     end
   end
 
